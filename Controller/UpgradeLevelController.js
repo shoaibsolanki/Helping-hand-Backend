152c31f293b8c683d6dadeb77934cfbc77a6aef6
Veedel <hot.sa.chhora@gmail.com>
2024-07-24 20:24:39 +0530

    levelupdate

--- Controller/UpgradeLevelController.js
+++ Controller/UpgradeLevelController.js
@@ -1,14 +1,59 @@
 const User = require("../models/User");
-const Joi = require('joi');
+const Joi = require("joi");
 
-class UpgradeLevelController{
-    static async Upgradelevel(req,res){
+class UpgradeLevelController {
+  static async Upgradelevel(req, res) {
+    try {
+      // Validate the request body
+      const schema = Joi.object({
+        user_id: Joi.string().required(),
+      });
+
+      const { error } = schema.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+
+      const { user_id } = req.body;
+
+      // Find the user by user_id
+      const user = await User.findOne({ user_id });
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      // Increment the user's level
+      if (user.level < 10) {
         try {
-            
-        } catch (error) {
-            console.log(error)
-        }
-    }
+          const newLevel = (parseInt(user.level, 10) + 1).toString();
+          user.level = newLevel;
 
+          await user.save();
+          console.log(`User ${user_id} level updated to ${newLevel}`);
+
+          res.status(200).json({
+            message: "User level upgraded successfully",
+            user,
+          });
+        } catch (err) {
+          console.error(`Error saving user ${user_id}:`, err);
+          res.status(500).json({
+            message: "Error upgrading user level",
+            error: err.message,
+          });
+        }
+      } else {
+        res.status(400).json({
+          message: "User level is already above 10",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        error: "An error occurred while upgrading the user level",
+      });
+    }
+  }
 }
+
 module.exports = UpgradeLevelController;
--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const UserController = require('../Controller/UserController');
-const fetchuser = require('../middleware/fetchuser')
-const RoleBase = require('../middleware/RoleBase')
-class AuthRoutes{
-    constructor() { 
-        this.router = express.Router();
-        this.getRoutes();
-        this.postRoutes();
-        this.patchRoutes();
-        this.deleteRoutes();
-    }
-     
-    getRoutes(){
-      this.router.get('/getmylevel/:userId',fetchuser, UserController.TeamLevel)
-    }
-    postRoutes(){
-        this.router.post('/UserRegister', UserController.CreateUser)
-        this.router.post('/CreatAdmin', UserController.CreateAdmin)
-        this.router.post('/CreatAgent',fetchuser,RoleBase('Admin'),UserController.AgentCreatedByAdmin)
-        this.router.post('/Login', UserController.Login)
-    }
-    patchRoutes(){
+const express = require("express");
+const UserController = require("../Controller/UserController");
+const fetchuser = require("../middleware/fetchuser");
+const RoleBase = require("../middleware/RoleBase");
+const UpgradeLevelController = require("../Controller/UpgradeLevelController");
+class AuthRoutes {
+  constructor() {
+    this.router = express.Router();
+    this.getRoutes();
+    this.postRoutes();
+    this.patchRoutes();
+    this.deleteRoutes();
+  }
 
-    }
-    deleteRoutes(){
-        
-    }
-
-
+  getRoutes() {
+    this.router.get("/getmylevel/:userId", fetchuser, UserController.TeamLevel);
+  }
+  postRoutes() {
+    this.router.post("/UserRegister", UserController.CreateUser);
+    this.router.post("/CreatAdmin", UserController.CreateAdmin);
+    this.router.post(
+      "/CreatAgent",
+      fetchuser,
+      RoleBase("Admin"),
+      UserController.AgentCreatedByAdmin
+    );
+    this.router.post("/Login", UserController.Login);
+    this.router.post("/levelUpgrade", UpgradeLevelController.Upgradelevel);
+  }
+  patchRoutes() {}
+  deleteRoutes() {}
 }
-module.exports = new AuthRoutes().router
+module.exports = new AuthRoutes().router;
